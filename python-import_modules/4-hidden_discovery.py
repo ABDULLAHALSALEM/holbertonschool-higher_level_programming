3829b308c2b8ed9253aedca744674cf890189a80
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2025-11-29 14:58:44 +0000

    Fix main condition

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     def_names = dir(hidden_4)
 
-    for name in sorted(def_names):
-        if not name.startswith("__"):
-            print(name)
+    for i in range(len(def_names)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
