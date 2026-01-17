7fa639a0a8228a1849821ecde2c1bb8ecf8787d6
ABDULLAHALSALEM <Abdullah.m122144@gmail.com>
2026-01-17 20:05:29 +0000

    updat

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
-""" Lists all states from the database hbtn_0e_0_usa """
+"""
+0-select_states.py
 
+Lists all states from the database hbtn_0e_0_usa.
+Results are sorted by states.id ASC.
+"""
+
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
+
+def main():
+    """Connect to MySQL and list all states ordered by id."""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
+        user=username,
+        passwd=password,
+        db=db_name,
         charset="utf8"
     )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
 
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
+
